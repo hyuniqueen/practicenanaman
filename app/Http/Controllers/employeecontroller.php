8ef29bbdfe5d1e45bcce64675e801c3586531738
unknown <balandoallysa02@gmail.com>
2025-05-29 17:56:53 +0800

    eto na

--- app/Http/Controllers/employeecontroller.php
+++ app/Http/Controllers/employeecontroller.php
@@ -39,18 +39,18 @@ class employeecontroller extends Controller
     public function edit( int $id)
     {
         $employees = employee::find($id);
-        return view ('employee.edit');
+        return view ('employee.edit', compact('employees'));
     }
 
     public function update(Request $request, int $id) {
         {
             $request->validate([
-                'fname' => 'required|max:255|mama ko',
-                'lname' => 'required|max:255|papa ko',
-                'midname' => 'required|max:255|ate ko',
-                'age' => 'required| tita ko',
-                'address' => 'required|max:255|tito ko',
-                'zip' => 'required| pamilya ko',
+                'fname' => 'required|max:255|string',
+                'lname' => 'required|max:255|string',
+                'midname' => 'required|max:255|string',
+                'age' => 'required| integer',
+                'address' => 'required|max:255|string',
+                'zip' => 'required| integer',
 
             ]);
 
